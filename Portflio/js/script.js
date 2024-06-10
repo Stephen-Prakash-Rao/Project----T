56a416fc39ff1e3d738fa4e83bf35dcd576a0220
Stephen_Prakash_Rao <sokttiroy@gmail.com>
2024-06-10 09:07:36 +0600

    Protflio navbar scrolling effect added

--- Portflio/js/script.js
+++ Portflio/js/script.js
@@ -5,3 +5,10 @@ menuIcon.addEventListener("click",function () {
   menuItem.classList.toggle("hidden");
 
 });
+
+window.addEventListener("scroll",()=>{
+
+  let nav = document.getElementById("navigationBar");
+  nav.classList.toggle("navigationBar", window.scrollY > 100);
+
+});
